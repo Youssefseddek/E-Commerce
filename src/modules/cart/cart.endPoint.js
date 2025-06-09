0906c14da0a49e1a606d580f88fc93e9f94de7df
youssef <ym658089@gmail.com>
2025-06-09 22:23:41 +0300

    edit3

--- src/modules/cart/cart.endPoint.js
+++ src/modules/cart/cart.endPoint.js
@@ -6,6 +6,6 @@ import { roles } from "../../middleware/authentication.js";
 export const endPoint = {
     add :[roles.Admin,roles.User],
     remove :[roles.Admin,roles.User],
-    clear :[roles.Admin,roles.User]
+    clear :[roles.Admin,roles.User],
     get :[roles.Admin,roles.User]
 }
